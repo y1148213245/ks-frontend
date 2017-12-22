d34b96ffc2a9a3c2357557714144489928336fea
yan.chaoming_company <yanchaoming136@163.com>
2017-12-22 16:01:55 +0800

    Signed-off-by: yan.chaoming_company <yanchaoming136@163.com>

---
+++ src/projects/zhixiang/components/bookdetail/handleUrl.vue
@@ -0,0 +1,21 @@
+<template>
+
+</template>
+
+<script type="text/ecmascript-6">
+  import URL from "url";
+
+  export default {
+    name: "components_bookdetail_handleurl",
+    reused: true,
+    mounted: function () {
+      var query = URL.parse(document.URL, true).query;
+      this.$bus.$emit("relativebook", query.pubId);
+      // this.$bus.$emit("historylist", query.contentType);
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/projects/zhixiang/main/filter.js
+++ src/projects/zhixiang/main/filter.js
@@ -2,5 +2,8 @@ import Vue from 'vue';
 // 注册
 Vue.filter('not-available', function (value) {
   // 返回处理后的值
-  return '暂无';
+  if(!value){
+    return '暂无';
+  }
+  return value;
 })
